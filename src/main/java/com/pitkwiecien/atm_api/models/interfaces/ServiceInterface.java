ea7f84fe894d4df26d812227d8e6d3abd5545ee0
pitkwiecien <pitkwiecien@gmail.com>
2022-12-11 23:25:54 +0100

    Changed ServiceInterface.verify() signature Added hint to commented .verify() for programmers knowledge (impossible to implement this in java)

--- src/main/java/com/pitkwiecien/atm_api/models/interfaces/ServiceInterface.java
+++ src/main/java/com/pitkwiecien/atm_api/models/interfaces/ServiceInterface.java
@@ -1,7 +1,7 @@
 package com.pitkwiecien.atm_api.models.interfaces;
 
 public interface ServiceInterface {
-//    int verify();
+//    int verify(ANY NEEDED REPOSITORIES);
     @SuppressWarnings("BooleanMethodIsAlwaysInverted")
     boolean verifyNotNulledParams();
     @SuppressWarnings("BooleanMethodIsAlwaysInverted")
